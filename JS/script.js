45bf18de3cec5e22205ac7cdc79d12b52ff48679
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-06-01 18:02:20 +0300

    Rest testailua yhteystietohin ja JS

--- JS/script.js
+++ JS/script.js
@@ -14,3 +14,26 @@ fetch('https://yesno.wtf/api')
 function frontpage() {
     location.href="index.html";
 }
+
+
+// set endpoint and your access key
+endpoint = 'latest'
+access_key = 'lx3c5h6c9w4zhau3l8j689my4s92gw9zr0qms27ncb7sfntej45pp60qouo5';
+
+// get the most recent exchange rates via the "latest" endpoint:
+$.ajax({
+url: 'https://metals-api.com/api/' + endpoint + '?access_key=' + access_key,   
+dataType: 'jsonp',
+success: function(json) {
+
+// exchange rata data is stored in json.rates
+alert(json.rates.EUR);
+
+// base currency is stored in json.base
+alert(json.base);
+
+// timestamp can be accessed in json.timestamp
+alert(json.timestamp);
+
+}
+});
