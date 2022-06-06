2412efe1a6a86abbcddf0fb93abe80cded8426f7
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-06-07 01:28:23 +0300

    Tekstin ja värin vaihdoin kokeiluja JS

--- JS/script.js
+++ JS/script.js
@@ -46,7 +46,7 @@ function frontPage() {
 let button1= document.getElementById("home");
 let button2= document.getElementById("back");
 
-button1.addEventListener("click",alkuun );
+button1.addEventListener("click",alkuun);
 button2.addEventListener("click",takaisin);
 
 function alkuun(){
@@ -54,31 +54,31 @@ function alkuun(){
 }
 
 function takaisin(){
-  location.href="about.html";
+  location.href="yhteystiedot.html";
+}
+/*
+//tekstin ja värin vaihto
+document.querySelector("h3").textContent=" Tilaa tuotteita, anna palautetta, tiedustele tilaus- ja korjaustöistä!";
+let h3= document.querySelector("h3");
+h3.style.color= "pink";
+
+//Tekstin värinvaihto kun hiiri on tekstin päällä
+let p = document.querySelector("p");
+p.addEventListener("mouseenter",changeColor);
+p.addEventListener("mouseleave",changeColor);
+
+function changeColor(){
+    p.classList.toggle("textcolor");
 }
 
+//tekstin värin vaihto
+let m = document.getElementById("clearfix");
+m.style.color = "teal";
+
+//taustan värin vaihto
+let n = document.getElementById("clearfix");
+n.style.backgroundColor = "beige";*/
 
 /*värinvaihtoa?
 let aloitus = document.querySelector("#aloitusbox>card-body");
-/*
-// set endpoint and your access key
-endpoint = 'latest'
-access_key = 'lx3c5h6c9w4zhau3l8j689my4s92gw9zr0qms27ncb7sfntej45pp60qouo5';
-
-// get the most recent exchange rates via the "latest" endpoint:
-$.ajax({
-url: 'https://metals-api.com/api/' + endpoint + '?access_key=' + access_key,   
-dataType: 'jsonp',
-success: function(json) {
-
-// exchange rata data is stored in json.rates
-alert(json.rates.EUR);
-
-// base currency is stored in json.base
-alert(json.base);
-
-// timestamp can be accessed in json.timestamp
-alert(json.timestamp);
-
-}
-});*/
+*/
