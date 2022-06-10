f54c0dbbbc170e363b9215ebd48187f512f1fa8f
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-06-10 21:45:19 +0300

    tekstin värin muuttaminen JS

---
+++ JS/code.js
@@ -0,0 +1,10 @@
+//h2 otsikon värinmuutos kun hiiri menee tekstin päälle.
+let title = document.getElementById("heading2");
+
+title.addEventListener("mouseenter", changeColorCard);
+title.addEventListener("mouseleave", changeColorCard);
+
+//hiirellä tekstin päälle, vaihtuu väri
+function changeColorCard(){
+  title.classList.toggle("textcolor");
+}
